f0c461dd6da35a3ead7c837de84e9ca7d643f57e
unknown <samello688@misena.edu.co>
2022-08-03 16:00:04 -0500

    Pass update and instructor fixes

--- public/Models/recover_pass_validation.php
+++ public/Models/recover_pass_validation.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="../Views/css/signup.css">
+            <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Validación de Recuperación</title>
+</head>
+<body>
 <?php
     require('connection.php');
 
@@ -12,13 +23,44 @@
         $birthYear = stripslashes($_REQUEST['birthYear']);
         $birthYear = mysqli_real_escape_string($dbConnection, $birthYear);
     
-        $recover_query = "UPDATE persona SET contraseña = '$pass' WHERE correo_electronico = '$email' AND fecha_nacimiento = '$birthYear'";
-        $query_result = mysqli_query($dbConnection, $recover_query);
+        $confirm_query = "SELECT * FROM persona WHERE correo_electronico = '$email' AND fecha_nacimiento = '$birthYear'";
+        $confirm_result = mysqli_query($dbConnection, $confirm_query) or die(mysqli_errno($dbConnection));
+        $confirm_array = mysqli_fetch_all($confirm_result);
 
-        if ($query_result){
-            echo 'Contraseña actualizada correctamente.';
-        } else {
-            echo 'No se pudo actualizar la contraseña.';
+        $recover_query = "UPDATE persona SET contraseña = '$pass' WHERE correo_electronico = '$email' AND fecha_nacimiento = '$birthYear'";
+        $query_result = mysqli_query($dbConnection, $recover_query) or die(mysqli_error($dbConnection));
+
+        if ($confirm_array == null) {
+            ?>
+            <script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Los datos ingresados no corresponden.'
+                }).then(function() {
+                    history.back();
+                });
+            </script>
+        </body>
+        </html>
+        <?php 
+        exit;   
+        } elseif ($confirm_array[0][5] === $email && $confirm_array[0][3] === $birthYear) {
+            ?>
+            <script>
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Actualización completada',
+                    text: 'Su contraseña se actualizó.'
+                }).then(function() {
+                    window.location.href = "../Views/login.php";
+                });
+            </script>
+        </body>
+        </html>
+        <?php
         }
     }
 ?>
+</body>
+</html>
--- public/Views/Instructor/instructor.php
+++ public/Views/Instructor/instructor.php
@@ -16,13 +16,43 @@
       $read_query = "SELECT *  FROM persona WHERE ID_Persona = '$id'";
       $query_result = mysqli_query($dbConnection, $read_query) or die(mysqli_error($dbConnection));
       $result_array = mysqli_fetch_all($query_result, MYSQLI_NUM);
+
+      $program_query = "SELECT * FROM programa_formacion";
+      $program_result= mysqli_query($dbConnection, $program_query) or die(mysqli_error($dbConnection));
+      $program_array = mysqli_fetch_all($program_result, MYSQLI_NUM);
+
+      $get_info_query = "SELECT 
+            P.nombres,
+            P.apellidos,
+            P.telefono,
+            P.rol,
+            A.ID_Persona,
+            A.ID_Programa,
+            A.ID_Ficha
+        FROM persona P
+        JOIN ambiente_virtual A
+        ON P.ID_Persona = A.ID_Persona";
+      $get_info_result = mysqli_query($dbConnection, $get_info_query) or die(mysqli_error($dbConnection));
+      $get_info_array = mysqli_fetch_all($get_info_result, MYSQLI_NUM);
+      $ficha = $get_info_array[0][5];
     ?>
     <?php include './blocks/sidebar.php' ?>
         <h1 class="main-content__header">Bienvenido Instructor 👋</h1>
         <div class="main-content__info">
-          ...
+          <p>Ficha: </p><?php echo $get_info_array[0][5];?>
+          <p>Programa de formación: </p><?php echo $program_array[0][1] ?>
+          <p>Aprendices: <br><?php 
+            for ($i=0; $i < count($get_info_array); $i++) {
+              if ($get_info_array[$i][5] != $ficha) {
+                echo "NUEVO";
+                exit;
+              } else {
+                echo $get_info_array[$i][4].' '.$get_info_array[$i][0].' '.$get_info_array[$i][1].' '.$get_info_array[$i][2]; ?><br>
+                <?php
+              }
+            } ?></p>
         </div>
-      </main>
+      </main> 
     </div>
     <script src="../../Controllers/instructor-control.js"></script>
   </body>
