ed98e9990b0595ed1462edf44f80ad548cb01e06
unknown <sebasti.ramos08@gmail.com>
2022-08-26 13:02:39 -0500

    Working in <See Pass> function

---
+++ public/Controllers/pass-visualization.js
@@ -0,0 +1,9 @@
+const passField = document.querySelector('.pass-input');
+const seeBtn = document.querySelector('.see-btn');
+const unSeeBtn = document.querySelector('.unsee-btn');
+
+// FIXME
+// unSeeBtn.addEventListener('click', function () {
+//   unSeeBtn.styles.display = 'none !important';
+//   seeBtn.styles.display = 'block !important';
+// });
--- public/Views/login.php
+++ public/Views/login.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="./css/login.css" />
     <link rel="icon" type="image/x-icon" href="./img/favicon.ico" />
     <script src="https://kit.fontawesome.com/643b0ccc65.js" crossorigin="anonymous"></script>
-    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Inicio de Sesión</title>
   </head>
@@ -17,7 +16,11 @@
         <h1 class="login__title">Inicio de Sesión</h1>
         <form action="../Models/login_validation.php" method="POST" class="login__form">
           <input type="email" name="email" id="email" placeholder="Correo electrónico" class="register-input" value="<?php echo htmlspecialchars($_POST['email'] ?? '', ENT_QUOTES);?>" required />
-          <input type="password" name="pass" placeholder="Contraseña" class="register-input" required />
+          <div class="pass__field">
+            <input type="password" name="pass" placeholder="Contraseña" class="register-input pass-input" required />
+            <i class="fa-solid fa-eye eye see-btn"></i>
+            <i class="fa-solid fa-eye-slash eye unsee-btn"></i>
+          </div>
           <button type="submit" name="login" class="login__btn">Ingresar</button>
         </form>
       </div>
@@ -29,5 +32,6 @@
         <a href="./recover-pass.php">¿Se te olvidó la contraseña?</a>
       </div>
     </div>
+    <script src="../Controllers/pass-visualization.js"></script>
   </body>
 </html>
