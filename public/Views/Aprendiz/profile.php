be650ffcb96f84b6b80f02b62c61719aceaa0f1b
SebastianRamos11 <sebasti.ramos08@gmail.com>
2022-08-30 10:11:58 -0500

    Working in Publication Center

--- public/Views/Aprendiz/profile.php
+++ public/Views/Aprendiz/profile.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../css/instructor.css" />
+    <link rel="icon" type="image/x-icon" href="../img/favicon.ico" />
     <script src="https://kit.fontawesome.com/643b0ccc65.js" crossorigin="anonymous"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <title>Document</title>
+    <title>Mi Perfil</title>
   </head>
   <body>
   <?php
--- public/Views/Instructor/upload.php
+++ public/Views/Instructor/upload.php
@@ -3,13 +3,12 @@ include_once "../../Models/connection.php";
   include_once "../../Models/session.php";
 if($_POST['submit']){
     if(file_exists($_FILES['file']['tmp_name'])){
-        if(move_uploaded_file($_FILES['file']['tmp_name'], 'upload/'.$_FILES['file']['name'])){
+        if(move_uploaded_file($_FILES['file']['tmp_name'], '../file-store/'.$_FILES['file']['name'])){
             $asunto = $_POST['asunto'];
-            $url = 'upload/'.$_FILES['file']['name'];
             $fecha = $_POST['fecha_pub'];
+            $url = '../file-store/'.$_FILES['file']['name'];
             $tipo = $_POST['tipo_p'];
-
-            $sql = $dbConnection->query("INSERT INTO publi (asunto,direccion,fecha,tipo_publicacion) VALUES ('".$asunto."', '".$url."', '".$fecha."', '".$tipo."')");
+            $sql = $dbConnection->query("INSERT INTO publicacion (ID_Persona,asunto,fecha,tipo_publicacion,url) VALUES ('".$session."', '".$asunto."', '".$fecha."', '".$tipo."', '".$url."')");
             echo "Archivos subidos con exito";
         }else{
             echo "Archivos no subidos vuelva a intentar";
