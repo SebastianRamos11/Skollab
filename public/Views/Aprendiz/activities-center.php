b1ebcd45d097c5afecc3a6f099f4fdf708141c0f
Sebastian Ramos <sebasti.ramos08@gmail.com>
2022-12-12 13:37:25 -0500

    Queries of student  module fixed

--- public/Views/Aprendiz/activities-center.php
+++ public/Views/Aprendiz/activities-center.php
@@ -2,13 +2,9 @@
   include_once "../../Models/connection.php";
   session_start();
   if (isset($_SESSION['id'])) {
-    $subject_query = "SELECT * FROM materia";
-    $subject_result= mysqli_query($dbConnection, $subject_query) or die(mysqli_error($dbConnection));
-    $subject_array = mysqli_fetch_all($subject_result, MYSQLI_NUM);
-    
-    $course = "SELECT * FROM ambiente_virtual WHERE ID_Persona =".$_SESSION['id'];
-    $course_result = mysqli_query($dbConnection, $course) or die(mysqli_error($dbConnection));
-    $course_array = mysqli_fetch_all($course_result, MYSQLI_NUM);
+    $groups = "SELECT ID_Ficha FROM ambiente_virtual WHERE ID_Persona =".$_SESSION['id'];
+    $groups_result = mysqli_query($dbConnection, $groups) or die(mysqli_error($dbConnection));
+    $groups = mysqli_fetch_all($groups_result, MYSQLI_NUM);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -26,79 +22,97 @@
       <h1 class="main-content__header">📚 Centro de actividades</h1>
       <div>
         <?php
-          for($i = 0; $i < sizeof($course_array); $i++){
-            $subject = $course_array[$i][1];
-            $ficha = $course_array[$i][2]; 
+          for($i = 0; $i < sizeof($groups); $i++){
+            $ficha = $groups[$i][0]; 
+            
+            // GET COURSE'S SUBJECTS
+            $course = "SELECT C.ID_Materia, C.ID_Instructor, F.numero FROM curso C JOIN ficha F ON C.ID_Ficha = F.ID_Ficha WHERE C.ID_Ficha = $ficha";
+            $course_result = mysqli_query($dbConnection, $course) or die(mysqli_error($dbConnection));
+            $course = mysqli_fetch_all($course_result, MYSQLI_NUM);
+					  
+            $course_num = $course[$i][2];
 
-            // GET PROGRAM NAME
-            $get_program = "SELECT nombre FROM materia WHERE ID_Materia = '$subject'";
-            $get_program_result = mysqli_query($dbConnection, $get_program) or die(mysqli_error($dbConnection));
-            $get_program_array = mysqli_fetch_all($get_program_result, MYSQLI_NUM);
-
-
-            // GET ACTIVITIES ID'S (TO LOOP)
-            $act = "SELECT AC.ID_Actividad FROM actividad AC JOIN ambiente_virtual A ON AC.ID_Persona = A.ID_Persona WHERE AC.ID_Ficha = $ficha AND A.ID_Materia = $subject;";
-            $act_result = mysqli_query($dbConnection, $act) or die(mysqli_error($dbConnection));
-            $act_array = mysqli_fetch_all($act_result, MYSQLI_NUM);
-            $activities = [];
-            for($j=0; $j < sizeof($act_array); $j++) array_push($activities, $act_array[$j][0]);
-
-            // EVIDENCES (ID'S) ARRAY
-            $evi = "SELECT ID_Actividad FROM evidencia WHERE ID_Persona = $id_user";
-            $evi_result = mysqli_query($dbConnection, $evi) or die(mysqli_error($dbConnection));
-            $evi_array = mysqli_fetch_all($evi_result, MYSQLI_NUM);
-            $evidences = [];
-            for($j=0; $j < sizeof($evi_array); $j++) array_push($evidences, $evi_array[$j][0]);
-
-            // CREATE DEFINITIVE ACTIVITIES TO SHOW
-            $pending = array_merge(array(), $activities);
-            for($j = 0; $j < sizeof($evidences); $j++){
-              if(in_array($evidences[$j], $activities)){
-                $elem = array_search($evidences[$j], $pending);
-                array_splice($pending, $elem, 1);
-              }
-            }
             ?>
-            <div class="activities-course">
-              <div class="activities-course__label"><?php echo $get_program_array[0][0]; ?></div>
+            <div class="user-course">
+              <h2 class="user-course__name">Curso <?php echo $course_num ?></h2>
               <hr>
               <?php
-                if (sizeof($pending) > 0){
+							  for($j = 0; $j < sizeof($course); $j++){
+                  $id_subject = $course[$j][0];
+                  $id_instructor = $course[$j][1];
+
+                  // GET SUBJECT
+                  $subject = "SELECT nombre FROM materia WHERE ID_Materia = $id_subject";
+                  $subject_result= mysqli_query($dbConnection, $subject) or die(mysqli_error($dbConnection));
+                  $subject = mysqli_fetch_all($subject_result, MYSQLI_NUM)[0][0];
+
+                  // GET ACTIVITIES ID'S (TO LOOP)
+                  $act = "SELECT ID_Actividad, asunto, fecha FROM `actividad` WHERE ID_Ficha = $ficha AND ID_Persona = $id_instructor";
+                  $act_result = mysqli_query($dbConnection, $act) or die(mysqli_error($dbConnection));
+                  $act_array = mysqli_fetch_all($act_result, MYSQLI_NUM);
+                  $activities = [];
+                  for($k=0; $k < sizeof($act_array); $k++) array_push($activities, $act_array[$k][0]);
+
+                  // EVIDENCES (ID'S) ARRAY
+                  $evi = "SELECT ID_Actividad FROM evidencia WHERE ID_Persona = $id_user";
+                  $evi_result = mysqli_query($dbConnection, $evi) or die(mysqli_error($dbConnection));
+                  $evi_array = mysqli_fetch_all($evi_result, MYSQLI_NUM);
+                  $evidences = [];
+                  for($k=0; $k < sizeof($evi_array); $k++) array_push($evidences, $evi_array[$k][0]);
+
+                  // CREATE DEFINITIVE ACTIVITIES TO SHOW
+                  $pending = array_merge(array(), $activities);
+                  for($k = 0; $k < sizeof($evidences); $k++){
+                    if(in_array($evidences[$k], $activities)){
+                      $elem = array_search($evidences[$k], $pending);
+                      array_splice($pending, $elem, 1);
+                    }
+                  }
                   ?>
-                  <div class="activities">
+                  <div class="activities-course">
+                    <div class="activities-course__label"><?php echo $subject; ?></div>
+                    <hr>
                     <?php
-                      for($j=0; $j < sizeof($pending); $j++){
-                        $id_activity = $pending[$j];
-                        $activity = "SELECT AC.ID_Actividad, AC.asunto, AC.descripcion, AC.fecha, AC.fecha_limite, AC.url, A.ID_Materia FROM actividad AC JOIN ambiente_virtual A ON AC.ID_Persona = A.ID_Persona WHERE AC.ID_Ficha = $ficha AND AC.ID_Actividad = $id_activity AND A.ID_Materia = $subject;";
-                        $activity_result = mysqli_query($dbConnection, $activity) or die(mysqli_error($dbConnection));
-                        $activity_array = mysqli_fetch_all($activity_result, MYSQLI_NUM);
+                      if (sizeof($pending) > 0){
                         ?>
-                        <div class="activity">
-                          <div class="activity__title"><?php print_r($activity_array[0][1]); ?></div>
-                          <div class="activity__date">Fecha actividad: <?php print_r($activity_array[0][3]); ?></div>
-                          <div class="activity__info">
-                            <div class="activity__p"><?php print_r($activity_array[0][2]); ?></div>
-                            <div class="activity__date-limit"><?php print_r($activity_array[0][4]); ?></div>
-                            <div class="activity__type">Actividad</div>
-                            <?php
-                              if($activity_array[0][5] != ''){
-                                ?>
-                                <a href="<?php print_r($activity_array[0][5]); ?>" class="activity__file" download=""><i class="fa-regular fa-file-lines"></i></a>
-                                <?php 
-                              }
-                            ?>
-                          </div>
-                          <div class="activity__btns">
-                            <a href="activity.php?activity=<?php echo $activity_array[0][0]?>" class="activity__btns-link">Entregar>></a>
-                          </div>
+                        <div class="activities">
+                          <?php
+                            for($k=0; $k < sizeof($pending); $k++){
+                              $id_activity = $pending[$k];
+                              $activity = "SELECT AC.ID_Actividad, AC.asunto, AC.descripcion, AC.fecha, AC.fecha_limite, AC.url FROM actividad AC JOIN ambiente_virtual A ON AC.ID_Persona = A.ID_Persona WHERE AC.ID_Ficha = $ficha AND AC.ID_Actividad = $id_activity;";
+                              $activity_result = mysqli_query($dbConnection, $activity) or die(mysqli_error($dbConnection));
+                              $activity_array = mysqli_fetch_all($activity_result, MYSQLI_NUM);
+                              ?>
+                              <div class="activity">
+                                <div class="activity__title"><?php print_r($activity_array[0][1]); ?></div>
+                                <div class="activity__date">Fecha actividad: <?php print_r($activity_array[0][3]); ?></div>
+                                <div class="activity__info">
+                                  <div class="activity__p"><?php print_r($activity_array[0][2]); ?></div>
+                                  <div class="activity__date-limit"><?php print_r($activity_array[0][4]); ?></div>
+                                  <div class="activity__type">Actividad</div>
+                                  <?php
+                                    if($activity_array[0][5] != ''){
+                                      ?>
+                                      <a href="<?php print_r($activity_array[0][5]); ?>" class="activity__file" download=""><i class="fa-regular fa-file-lines"></i></a>
+                                      <?php 
+                                    }
+                                  ?>
+                                </div>
+                                <div class="activity__btns">
+                                  <a href="activity.php?activity=<?php echo $activity_array[0][0]?>" class="activity__btns-link">Entregar>></a>
+                                </div>
+                              </div>
+                              <?php
+                            }
+                          ?>
                         </div>
                         <?php
+                      } else{
+                        ?><div class="good-message"><i class="fa-solid fa-check"></i> No hay evidencias por entregar.</div><?php
                       }
                     ?>
                   </div>
                   <?php
-                } else{
-                  ?><div class="good-message"><i class="fa-solid fa-check"></i> No hay evidencias por entregar.</div><?php
                 }
               ?>
             </div>
--- public/Views/Aprendiz/briefcase.php
+++ public/Views/Aprendiz/briefcase.php
@@ -4,9 +4,9 @@
   
   if (isset($_SESSION['id'])) {
     // GET AMBIENTE VIRTUAL
-    $course = "SELECT * FROM ambiente_virtual WHERE ID_Persona =".$_SESSION['id'];
-    $course_result = mysqli_query($dbConnection, $course) or die(mysqli_error($dbConnection));
-    $course_array = mysqli_fetch_all($course_result, MYSQLI_NUM);
+    $groups = "SELECT ID_Ficha FROM ambiente_virtual WHERE ID_Persona =".$_SESSION['id'];
+    $groups_result = mysqli_query($dbConnection, $groups) or die(mysqli_error($dbConnection));
+    $groups = mysqli_fetch_all($groups_result, MYSQLI_NUM);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -24,120 +24,136 @@
     <?php include './sidebar.php' ?>
       <h1 class="main-content__header">💼 Portafolio</h1>
       <?php 
-        for($i=0; $i<sizeof($course_array); $i++){
-          $subject = $course_array[$i][1];
-          $ficha = $course_array[$i][2];
+        for($i=0; $i<sizeof($groups); $i++){
+					$ficha = $groups[$i][0];
 
-          $subject_query = "SELECT nombre FROM materia WHERE ID_Materia = $subject";
-          $subject_result= mysqli_query($dbConnection, $subject_query) or die(mysqli_error($dbConnection));
-          $subject_name = mysqli_fetch_all($subject_result, MYSQLI_NUM)[0][0];
-          ?>
-          <a class="program program-<?php echo $i ?>" href="#briefcase-<?php echo $i ?>">
-            <img class="program__figure" src="../img/courses/sena-logo.png" alt="program">
-            <div class="program__id">
-              <div class="program__title"><?php echo $subject_name; ?></div>
-              <div class="program__group"><?php echo $ficha; ?></div>
-            </div>
-          </a>
-          <?php
-          // GET INSTRUCTOR BY AMBIENTE VIRTUAL
-          $instructor = "SELECT A.ID_Persona, P.nombres, P.apellidos,  A.ID_Ficha FROM persona P JOIN ambiente_virtual A ON P.ID_Persona = A.ID_Persona WHERE A.ID_Materia = '$subject' AND A.ID_Ficha = $ficha AND P.ID_Rol = 2";
-          $instructor_result = mysqli_query($dbConnection, $instructor) or die(mysqli_error($dbConnection));
-          $instructor_array = mysqli_fetch_all($instructor_result, MYSQLI_NUM);
-          $id_instructor = $instructor_array[0][0];
+					// GET COURSE DATA
+					$course = "SELECT C.ID_Materia, C.ID_Instructor, F.numero FROM curso C JOIN ficha F ON C.ID_Ficha = F.ID_Ficha WHERE C.ID_Ficha = $ficha";
+					$course_result = mysqli_query($dbConnection, $course) or die(mysqli_error($dbConnection));
+					$course = mysqli_fetch_all($course_result, MYSQLI_NUM);
 
-          // GET INSTRUCTOR'S activities
-          $activities = "SELECT ID_Actividad, asunto, fecha FROM `actividad` WHERE ID_Persona = $id_instructor AND ID_Ficha = $ficha";
-          $activities_result = mysqli_query($dbConnection, $activities) or die(mysqli_error($dbConnection));
-          $activities_array = mysqli_fetch_all($activities_result, MYSQLI_NUM);
-          ?>
-					<div class="briefcase hidden" id="briefcase-<?php echo $i ?>">
+					$course_num = $course[$i][2];
+
+					?>
+					<div class="user-course">
+						<h2 class="user-course__name">Curso <?php echo $course_num ?></h2>
+						<hr>
 						<?php
-							if(sizeof($activities_array) > 0){
-								for($j=0; $j < sizeof($activities_array); $j++){
-									$id_activity = $activities_array[$j][0];
-									$activity_title = $activities_array[$j][1];
-									
-									// GET EVIDENCES DELIVERED BY activity
-									$evidences = "SELECT fecha, nota, observacion, url, ID_Actividad, ID_Evidencia FROM `evidencia` WHERE ID_Persona =".$_SESSION['id']." AND ID_Actividad = $id_activity";
-									$evidences_result = mysqli_query($dbConnection, $evidences) or die(mysqli_error($dbConnection));
-									$evidences_array = mysqli_fetch_all($evidences_result, MYSQLI_NUM);
+							for($j = 0; $j < sizeof($course); $j++){
+								$id_subject = $course[$j][0];
+								$id_instructor = $course[$j][1];
 
-									if(sizeof($evidences_array) > 0){
-										?>
-										<div class="briefcase-evidence">
-											<div class="briefcase-evidence__activity">
-												<i class="fa-solid fa-book briefcase-evidence__icon"></i>
-												<div class="briefcase-evidence__title"><?php echo $activity_title; ?></div>
-											</div>
-											<div class="briefcase-evidence__date"><?php echo $evidences_array[0][0] ;?></div>
-											<div class="briefcase-evidence__grade">
-												<div class="briefcase-evidence__grade-value">
-													<?php
-														if($evidences_array[0][1] != ''){
-															?><span class="gradeValue"><?php echo $evidences_array[0][1] ?>/100</span><?php
-														} else{
-															echo "--/100";
-														}
+								// GET SUBJECT
+								$subject = "SELECT img, nombre FROM materia WHERE ID_Materia = $id_subject";
+								$subject_result= mysqli_query($dbConnection, $subject) or die(mysqli_error($dbConnection));
+								$subject = mysqli_fetch_all($subject_result, MYSQLI_NUM);
+								
+								// GET INSTRUCTOR'S ACTIVITIES
+								$activities = "SELECT ID_Actividad, asunto, fecha FROM `actividad` WHERE ID_Ficha = $ficha AND ID_Persona = $id_instructor";
+								$activities_result = mysqli_query($dbConnection, $activities) or die(mysqli_error($dbConnection));
+								$activities_array = mysqli_fetch_all($activities_result, MYSQLI_NUM);
+
+								?>
+								<!-- SUBJECT -->
+								<a class="program program-<?php echo $i ?>" href="#briefcase-<?php echo $j ?>">
+									<img class="program__figure" src="<?php echo $subject[0][0]; ?>" alt="program">
+									<div class="program__id">
+										<div class="program__title"><?php echo $subject[0][1]; ?></div>
+									</div>
+								</a>
+
+								<!-- SUBJECT'S ACTIVITIES-->
+								<div class="briefcase hidden" id="briefcase-<?php echo $i ?>">
+									<?php
+										if(sizeof($activities_array) > 0){
+											for($k=0; $k < sizeof($activities_array); $k++){
+												$id_activity = $activities_array[$k][0];
+												$activity_title = $activities_array[$k][1];
+												
+												// GET EVIDENCES DELIVERED BY activity
+												$evidences = "SELECT fecha, nota, observacion, url, ID_Actividad, ID_Evidencia FROM `evidencia` WHERE ID_Persona =".$_SESSION['id']." AND ID_Actividad = $id_activity";
+												$evidences_result = mysqli_query($dbConnection, $evidences) or die(mysqli_error($dbConnection));
+												$evidences_array = mysqli_fetch_all($evidences_result, MYSQLI_NUM);
+
+												if(sizeof($evidences_array) > 0){
 													?>
-												</div>
-												<div class="briefcase-evidence__grade-range">
-													<?php
-														if($evidences_array[0][1] != ''){
-															?>
-															<span class="<?php 
-																if(intval($evidences_array[0][1]) > 80){
-																		echo "grade-a";
-																}else if(intval($evidences_array[0][1]) > 60){
-																		echo "grade-b";
-																} else{
-																		echo "grade-d";
-																}
-																?>" style="width: <?php echo $evidences_array[0][1] ?>%;">
-															</span>
-															<?php
-														} else{
-															?><span style="width: 0;"></span><?php
-														}
-													?>
-												</div>
-											</div>
-											<div class="briefcase-evidence__icons">
-												<div class="briefcase-evidence__observation">
-													<i class="fa-regular fa-comment-dots"></i>
-													<div class="briefcase-evidence__observation-p">
-														<?php
-															if($evidences_array[0][2] != ''){
-																echo "1";
-															} else{
-																echo "0";
-															}
-														?>
+													<div class="briefcase-evidence">
+														<div class="briefcase-evidence__activity">
+															<i class="fa-solid fa-book briefcase-evidence__icon"></i>
+															<div class="briefcase-evidence__title"><?php echo $activity_title; ?></div>
+														</div>
+														<div class="briefcase-evidence__date"><?php echo $evidences_array[0][0] ;?></div>
+														<div class="briefcase-evidence__grade">
+															<div class="briefcase-evidence__grade-value">
+																<?php
+																	if($evidences_array[0][1] != ''){
+																		?><span class="gradeValue"><?php echo $evidences_array[0][1] ?>/100</span><?php
+																	} else{
+																		echo "--/100";
+																	}
+																?>
+															</div>
+															<div class="briefcase-evidence__grade-range">
+																<?php
+																	if($evidences_array[0][1] != ''){
+																		?>
+																		<span class="<?php 
+																			if(intval($evidences_array[0][1]) > 80){
+																					echo "grade-a";
+																			}else if(intval($evidences_array[0][1]) > 60){
+																					echo "grade-b";
+																			} else{
+																					echo "grade-d";
+																			}
+																			?>" style="width: <?php echo $evidences_array[0][1] ?>%;">
+																		</span>
+																		<?php
+																	} else{
+																		?><span style="width: 0;"></span><?php
+																	}
+																?>
+															</div>
+														</div>
+														<div class="briefcase-evidence__icons">
+															<div class="briefcase-evidence__observation">
+																<i class="fa-regular fa-comment-dots"></i>
+																<div class="briefcase-evidence__observation-p">
+																	<?php
+																		if($evidences_array[0][2] != ''){
+																			echo "1";
+																		} else{
+																			echo "0";
+																		}
+																	?>
+																</div>
+															</div>
+															<a href="activity.php?activity=<?php echo $evidences_array[0][4] ;?>" class="briefcase-evidence__link"><i class="fa-regular fa-eye"></i></a>
+															<a href="<?php echo $evidences_array[0][3] ;?>" class="briefcase-evidence__link" download=""><i class="fa-regular fa-file-lines"></i></a>
+															<?php if(!$evidences_array[0][1]) { ?> <a href="delete.php?evidence=<?php echo $evidences_array[0][5];?>&b=1" class="briefcase-evidence__link briefcase-evidence__link--highlight delete-button"><i class="fa-regular fa-trash-can"></i></a> <?php }?>
+														</div>
 													</div>
-												</div>
-												<a href="activity.php?activity=<?php echo $evidences_array[0][4] ;?>" class="briefcase-evidence__link"><i class="fa-regular fa-eye"></i></a>
-												<a href="<?php echo $evidences_array[0][3] ;?>" class="briefcase-evidence__link" download=""><i class="fa-regular fa-file-lines"></i></a>
-												<?php if(!$evidences_array[0][1]) { ?> <a href="delete.php?evidence=<?php echo $evidences_array[0][5];?>&b=1" class="briefcase-evidence__link briefcase-evidence__link--highlight delete-button"><i class="fa-regular fa-trash-can"></i></a> <?php }?>
-											</div>
-										</div>
-										<?php
-									} else{
-										?>
-										<div class="briefcase-evidence briefcase-evidence--empty">
-											<div class="briefcase-evidence__activity">
-												<i class="fa-solid fa-triangle-exclamation briefcase-evidence__icon"></i>
-												<div class="briefcase-evidence__title"><?php echo $activity_title ;?></div>
-											</div>
-											<div class="briefcase-evidence--empty__alert">SIN ENTREGA</div>
-										</div>
-										<?php
-									}
-								}
-							} else{
-								?><div class="briefcase-empty">No se han publicado actividades en esta materia.</div><?php
+													<?php
+												} else{
+													?>
+													<div class="briefcase-evidence briefcase-evidence--empty">
+														<div class="briefcase-evidence__activity">
+															<i class="fa-solid fa-triangle-exclamation briefcase-evidence__icon"></i>
+															<div class="briefcase-evidence__title"><?php echo $activity_title ;?></div>
+														</div>
+														<div class="briefcase-evidence--empty__alert">SIN ENTREGA</div>
+													</div>
+													<?php
+												}
+											}
+										} else{
+											?><div class="briefcase-empty">No se han publicado actividades en esta materia.</div><?php
+										}
+									?>
+									<hr class="program-divider">
+								</div>
+								<?php
 							}
 						?>
-						<hr class="program-divider">
 					</div>
 					<?php
         }
